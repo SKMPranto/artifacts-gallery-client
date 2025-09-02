d1cb6af89311d8e0f0ae53ea286c6d10c3de7d78
SKMPranto <prantokumar3463@gmail.com>
2025-09-02 18:04:04 +0600

    Banner section update 2.0

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,7 +4,7 @@ import Banner from "./Banner";
 const Home = () => {
   return (
     <div>
-      <div className="relative bg-[url('https://i.ibb.co.com/KpDpZpZv/Screenshot-2025-09-02-174856.jpg')] bg-cover bg-center mt-5 pl-2 rounded-2xl">
+      <div className="relative bg-[url('https://i.ibb.co.com/KpDpZpZv/Screenshot-2025-09-02-174856.jpg')] bg-cover bg-center mt-5 md:pl-2 rounded-2xl">
         {/* Overlay */}
         <div className="absolute inset-0 bg-black/50 rounded-2xl"></div>
 
